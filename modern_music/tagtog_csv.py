ba4641e10161c571a34c9f67ef1784fb615c9e79
Doohae <rick7213@gmail.com>
2021-11-16 14:57:50 +0000

    tagtog_csv.py updating

--- modern_music/tagtog_csv.py
+++ modern_music/tagtog_csv.py
@@ -12,6 +12,10 @@ genre_json = os.path.join(folder, "ann.json/master/pool/")
 genre_html = os.path.join(folder, "plain.html/pool/")
 ###################################
 
+# entity, relation 맵핑
+with open(os.path.join(folder, "annotations-legend.json"), "r") as f:
+    mapping = json.load(f)
+
 
 # json폴더와 html폴더 탐색하여 genre_list에 ("A.json", "A.html") 형식으로 저장
 genre_json_folder = os.listdir(genre_json)
@@ -26,6 +30,34 @@ for genre in genre_json_folder:
     added_list = [(j, h) for j, h in zip(json_sub_list, html_sub_list)]
     genre_list.extend(added_list)
 
+# html로부터 문장 추출하는 함수
+def get_context_from_html(html_file):
+    html_file = re.sub(r"\n","", html_file)
+    return re.findall("(<pre.+>)(.+)(</pre>)",html_file)[0][1]
 
-def sentence_entities(json_path, html_path):
+
+def get_random_entities():
     pass
+
+def relation_set():
+    pass
+
+
+# sentence, ent1, ent2, relation 묶음 만들기
+def get_sentence_re(json_path, html_path):
+    with open(html_path, "r") as f:
+        html_file = f.read()
+    sentence = get_context_from_html(html_file)
+
+    with open(json_path, "r") as f:
+        json_file = json.load(f)
+
+    try:
+        relations = json_file['relations']
+
+    except:
+        pass
+
+    
+
+
