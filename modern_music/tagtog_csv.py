45fa1ca719416915640660ab8f27771f47d0c9fc
Doohae <rick7213@gmail.com>
2021-11-16 15:39:20 +0000

    tagtog_csv.py keep updating

--- modern_music/tagtog_csv.py
+++ modern_music/tagtog_csv.py
@@ -39,8 +39,51 @@ def get_context_from_html(html_file):
 def get_random_entities():
     pass
 
-def relation_set():
-    pass
+
+# {"classId":"r_11",
+# "type":"linked",
+# "directed":false,
+# "entities":["s1s1v1|e_1|13,13","s1s1v1|e_5|33,34"],
+# "confidence":{"state":"pre-added","who":["user:Doohae"],"prob":1}}
+
+# 위 형태의 relation_dict와 문장을 입력으로 넣어줬을 때 train.csv 형식에 맞게 subject, object, relation 반환
+def relation_set(relation_dict, sentence, mapping=mapping):
+    relation = mapping[relation_dict['classId']]
+    sub_start_idx, sub_end_idx = tuple(map(int, relation_dict['entities'][0].split('|')[-1].split(',')))
+    obj_start_idx, obj_end_idx = tuple(map(int, relation_dict['entities'][1].split('|')[-1].split(',')))
+    sub_type = mapping[relation_dict['entities'][0].split('|')[1]]
+    obj_type = mapping[relation_dict['entities'][1].split('|')[1]]
+    sub_word = sentence[sub_start_idx:sub_end_idx+1]
+    obj_word = sentence[obj_start_idx:obj_end_idx+1]
+    return {'word':sub_word, 'start_idx':sub_start_idx, 'end_idx':sub_end_idx, 'type':sub_type}, \
+            {'word':obj_word, 'start_idx':obj_start_idx, 'end_idx':obj_end_idx, 'type':obj_type}, \
+            relation
+
+# [{'classId': 'e_1',
+#   'part': 's1s1v1',
+#   'offsets': [{'start': 0, 'text': '비밥'}],
+#   'coordinates': [],
+#   'confidence': {'state': 'pre-added', 'who': ['user:vgptnv'], 'prob': 1},
+#   'fields': {},
+#   'normalizations': {}},
+#  {'classId': 'e_1',
+#   'part': 's1s1v1',
+#   'offsets': [{'start': 3, 'text': 'bebop'}],
+#   'coordinates': [],
+#   'confidence': {'state': 'pre-added', 'who': ['user:vgptnv'], 'prob': 1},
+#   'fields': {},
+#   'normalizations': {}}]
+
+# "relations":[{"classId":"r_11",
+#                 "type":"linked",
+#                 "directed":false,
+#                 "entities":["s1s1v1|e_1|13,13","s1s1v1|e_5|33,34"],
+#                 "confidence":{"state":"pre-added","who":["user:Doohae"],"prob":1}},
+#             {"classId":"r_21",
+#                 "type":"linked",
+#                 "directed":false,
+#                 "entities":["s1s1v1|e_1|0,1","s1s1v1|e_1|3,7"],
+#                 "confidence":{"state":"pre-added","who":["user:Doohae"],"prob":1}}]
 
 
 # sentence, ent1, ent2, relation 묶음 만들기
@@ -48,16 +91,22 @@ def get_sentence_re(json_path, html_path):
     with open(html_path, "r") as f:
         html_file = f.read()
     sentence = get_context_from_html(html_file)
+    assert type(sentence)==type("abc"), "sentence type incorrect!"
 
     with open(json_path, "r") as f:
         json_file = json.load(f)
-
-    try:
-        relations = json_file['relations']
-
-    except:
-        pass
-
+    entities = json_file['entities']
+    ent_list = []
+    for ent in entities:
+        # {'word':'뉴질랜드', 'start_idx':0, 'end_idx':3, 'type':'ORG'} 형식으로 만들기
+        ent_type = mapping[ent['classId']]
+        word = ent['offsets'][0]['text']
+        start_idx = ent['offsets'][0]['start']
+        end_idx = start_idx + len(word) - 1
+        ent_list.append({'word':word, 'start_idx':start_idx, 'end_idx':end_idx, 'type':ent_type})
     
-
-
+    relations = json_file['relations']
+    if len(relations) == 0:
+        pass
+    else:
+        pass
