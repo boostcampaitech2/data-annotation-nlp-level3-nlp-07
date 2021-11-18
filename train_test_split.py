ff9b73c061d7ce480deb3fdcc105595b23feb5ce
Doohae <rick7213@gmail.com>
2021-11-18 14:42:42 +0000

    split & csv simplify

--- train_test_split.py
+++ train_test_split.py
@@ -7,18 +7,10 @@ SAVE_DIR = "./data/"
 RANDOM_SEED = 42
 
 df = pd.read_csv(FILE_NAME)
-try:
-    train, test = train_test_split(df, test_size=0.1, stratify=df['relation'], random_state=RANDOM_SEED)
-except:
-    train, test = train_test_split(df, test_size=0.1, stratify=df['label'], random_state=RANDOM_SEED)
+train, test = train_test_split(df, test_size=0.1, stratify=df['label'], random_state=RANDOM_SEED)
 
 if not os.path.exists(SAVE_DIR):
     os.mkdir(SAVE_DIR)
 
-try:
-    train.rename(columns={"relation":"label"}, inplace=True)
-    test.rename(columns={"relation":"label"}, inplace=True)
-except:
-    pass
 train.to_csv(os.path.join(SAVE_DIR, "train.csv"), index=False)
 test.to_csv(os.path.join(SAVE_DIR, "test.csv"), index=False)
